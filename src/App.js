254368a030b637bd251fc0a6ac23773679bf8fdb
Sanket-Kokude <80410919+Sanket-Kokude@users.noreply.github.com>
2021-05-22 17:58:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
-
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Products from './components/pages/FAQ';
+import SignUp from './components/pages/SignUp';
+import AdminLogin from './components/pages/AdminLogin';
+import ViewPastData from './components/pages/ViewPastData';
+import About from './components/pages/About';
+import ViewUsers from './components/pages/ViewUsers';
+import FAQ_1 from './components/pages/FAQ_1';
+import FAQ_2 from './components/pages/FAQ_2';
+import Answer from './components/pages/Answer';
+import calc from './components/pages/calc';
 function App() {
+  localStorage.setItem("city","IIITA");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={calc} />
+          <Route path='/ViewPastData' component={ViewPastData} />
+          <Route path='/FAQ' component={Products} />
+          <Route path='/FAQ_1' component={FAQ_1} />
+          <Route path='/FAQ_2' component={FAQ_2} />
+          <Route path='/AdminLogin' component={AdminLogin} />
+          <Route path='/SignUp' component={SignUp} />
+          <Route path='/About' component={About} />
+          <Route path='/ViewUsers' component={ViewUsers} />
+          <Route path='/Answer' component={Answer} />
+          <Route path='/calc' component={calc} />
+          
+
+        </Switch>
+      </Router>
+    </>
   );
 }
 
